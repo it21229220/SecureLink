950e60777536253852804f18e56f2052b64909e3
it21229220 <99592732+it21229220@users.noreply.github.com>
2024-05-19 16:48:35 +0530

    Add files via upload

---
+++ resources/js/ajax.js
@@ -0,0 +1,241 @@
+// Function to perform the Diffie-Hellman key exchange
+function ajaxDiffieHellman(){
+  let dh = new DiffieHellman();
+  let q;
+  let proots;
+  let prootsLength;
+  let a;
+  let xa;
+  let xb;
+  let ya, yb;
+  let _a, _b;
+
+  // Loop to find appropriate keys
+  for(;;){
+    q = dh.findRandomPrime(100, 10000);
+    proots = dh.findPrimitives(q);
+    prootsLength = dh.findPrimitives(q).length;
+    a = proots[Math.round(Math.random() * prootsLength)];
+    xa = (Math.random() * q - 1) + 2;
+    xb = (Math.random() * q - 1) + 2;
+
+    //Public Key Alice
+    ya = dh.mpmod(a, xa, q)
+    //Public Key Bob
+    yb = dh.mpmod(a, xb, q)
+    //Secret
+    _a = dh.mpmod(yb, xa, q)
+    _b = dh.mpmod(ya, xb, q)
+    if(_a == _b){
+      break;
+    }
+  }
+
+  // Create a new XMLHttpRequest object
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        if(this.responseText != "")
+          alert(this.responseText);
+      } else {
+          // alert(this.status + " " + this.readyState);
+      }
+  };
+  // Prepare the POST parameters
+  var params = "sid=" + senderId + "&rid=" + receiverId + "&skey=" + _a + "&rkey=" + _b;
+  xmlhttp.open("POST", "insert_both_keys.php", true);
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.send(params);
+}
+
+// Function to insert a new message via AJAX
+function ajaxInsertMessage(){
+  var messageText = document.getElementById('message-text').value;
+  var encMethod = document.getElementById('enc-method').value;
+  var encMethodId = getEncMethodId(encMethod);
+  var file = document.getElementById("upload-file");
+  var isFile = 0;
+  var fileName = "";
+
+  let c1;
+  let xa;
+  let q;
+  let es;
+
+  if(file.files.length != 0){
+    isFile = 1;
+    messageText = fileBytes;
+    fileName = file.files[0].name;
+  }
+
+  // If the selected encryption method is ElGamal
+  if(encMethodId == 3){
+    let gamal = new Gamal();
+    q = gamal.findRandomPrime();
+    let proots = gamal.findPrimitives(q);
+    let prootsLength = gamal.findPrimitives(q).length;
+    let a = proots[Math.floor(Math.random() * prootsLength)];
+    xa = Math.floor(Math.random() * q - 1) + 2;
+    let ya = gamal.mpmod(a, xa, q);
+    let ciphers = gamal.encrypt(q, a, ya, messageText);
+    c1 = ciphers[0];
+    messageText = ciphers[1]; // c2
+    es = ciphers[2];
+  }
+
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        if(this.responseText != "")
+          alert(this.responseText);
+      } else {
+          // alert(this.status + " " + this.readyState);
+      }
+  };
+  var params = "sid=" + senderId + "&rid=" + receiverId + "&msg=" + encodeURIComponent(messageText) + "&eid=" + encMethodId + "&isFile=" + isFile + "&fileName=" + fileName + 
+                "&c1Gamal=" + c1 + "&xaGamal=" + xa + "&qGamal=" + q + "&esGamal=" + es;
+  xmlhttp.open("POST", "insert_message.php", true); // Open a POST request to insert_message.php
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); // Set the request header
+  xmlhttp.send(params);
+}
+
+// function ajaxDisplayMessages(whichCase) {
+//   var xmlhttp = new XMLHttpRequest();
+//   xmlhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       if (this.responseText != "") {
+//         //console.log(this.responseText);
+//         messagesData = JSON.parse(this.responseText);
+//         //console.log(messagesData);
+//         //console.log('message:', messagesData);
+//         if (ctModalOpen == 0) {
+//           appendSecuredAlertBubble();
+//           ctModalOpen++;
+//         }
+//         for (let i = 0; i < messagesData.length; i++) {
+//           //console.log('Message Data:', messagesData[i]);
+//           let theMessage = messagesData[i][3];
+//           if (messagesData[i][2] == 3) {
+//             let gamal = new Gamal();
+//             theMessage = gamal.decrypt(parseInt(messagesData[i][8][0]), messagesData[i][8][1], parseInt(messagesData[i][8][2]), parseInt(messagesData[i][8][3]), messagesData[i][8][4]);
+//           }
+//           let flaggedAsPhishing = messagesData[i][9]; // Assume this is the flagged_as_phishing field
+//           console.log('Message:', theMessage, 'Flagged as phishing:', flaggedAsPhishing); // Log for debugging
+//           if (flaggedAsPhishing) {
+//             theMessage += " <strong>(Warning: This URL is flagged as phishing)</strong>";
+//           }
+//           appendNewChatBubble(messagesData[i][0], theMessage, messagesData[i][4], messagesData[i][5], messagesData[i][6], messagesData[i][7]);
+//         }
+//         if (messagesData.length > 0) {
+//           scrollToTheBottom();
+//         }
+//       }
+//     }
+//   };
+//   xmlhttp.open("GET", "display_messages.php?sid=" + senderId + "&rid=" + receiverId + "&whichCase=" + whichCase, true);
+//   xmlhttp.send();
+// }
+
+function ajaxDisplayMessages(whichCase) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          if (this.responseText != "") {
+              messagesData = JSON.parse(this.responseText);
+              if (ctModalOpen == 0) {
+                  appendSecuredAlertBubble();
+                  ctModalOpen++;
+              }
+              for (let i = 0; i < messagesData.length; i++) {
+                  let theMessage = messagesData[i][3];
+                  let flaggedAsPhishing = messagesData[i][9]; // Assume this is the flagged_as_phishing field
+                  //console.log('Message:', theMessage, 'Flagged as phishing:', flaggedAsPhishing); // Log for debugging
+                  if (messagesData[i][9] == 1) {
+
+                    theMessage += "  (Warning: This URL is flagged as phishing)";
+
+                  } else if (messagesData[i][9] == 0) {
+                      if (messagesData[i][2] == 3) {
+
+                          let gamal = new Gamal();
+                          theMessage = gamal.decrypt(parseInt(messagesData[i][8][0]), messagesData[i][8][1], parseInt(messagesData[i][8][2]), parseInt(messagesData[i][8][3]), messagesData[i][8][4]);
+
+                      }
+                  }
+                  appendNewChatBubble(messagesData[i][0], theMessage, messagesData[i][4], messagesData[i][5], messagesData[i][6], messagesData[i][7]);
+              }
+              if (messagesData.length > 0) {
+                  scrollToTheBottom();
+              }
+          }
+      }
+  };
+  xmlhttp.open("GET", "display_messages.php?sid=" + senderId + "&rid=" + receiverId + "&whichCase=" + whichCase, true);
+  xmlhttp.send();
+}
+
+function ajaxCheckReceiverStatus(whichStatus){
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        if(this.responseText != ""){
+          if(whichStatus == "modal"){
+            const isOnline = JSON.parse(this.responseText)[0];
+            const logoutTimestamp = JSON.parse(this.responseText)[1];
+
+            var lastSeen = document.getElementById('last-seen');
+            var receiverDot = document.getElementById('receiver-dot');
+            
+            if(isOnline == 1){
+              lastSeen.innerHTML = "Online";
+              receiverDot.classList.remove("text-secondary");
+              receiverDot.classList.add("text-success");
+              chatButtonEvent.setAttribute('data-bs-status', '0');
+            }else{
+              lastSeen.innerHTML = "Last Seen " + logoutTimestamp;
+              receiverDot.classList.remove("text-success");
+              receiverDot.classList.add("text-secondary");
+              chatButtonEvent.setAttribute('data-bs-status', '0');
+            }
+          } else if (whichStatus == "home"){
+            const usersStatus = JSON.parse(this.responseText);
+            for(let i = 0; i < usersStatus.length; i++){
+              updateUsersPublicStatus(usersStatus[i], i);
+            }
+          }
+        }
+      } else {
+          // alert(this.status + " " + this.readyState);
+      }
+  };
+  xmlhttp.open("GET", "check_receiver_status.php?rid=" + receiverId + "&whichStatus=" + whichStatus, true);
+  xmlhttp.send();
+}
+
+function ajaxTypingReady(){
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        if(this.responseText != ""){
+        }
+      } else {
+          //alert(this.status + " " + this.readyState);
+      }
+  };
+  xmlhttp.open("GET", "typing_ready.php?rid=" + receiverId, true);
+  xmlhttp.send();
+}
+
+function ajaxTypingProcess(){
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        if(this.responseText != ""){
+        }
+      } else {
+          //alert(this.status + " " + this.readyState);
+      }
+  };
+  xmlhttp.open("GET", "typing_process.php?rid=" + receiverId, true);
+  xmlhttp.send();
+}
